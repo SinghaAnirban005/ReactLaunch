7e18f36b1e8325b1d9e9eb823eb9b7c8f8f8f42b
SinghaAnirban005 <anirbansingha20@gmail.com>
2025-07-02 16:20:12 +0530

    backend: Fetch all user projects

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -36,3 +36,26 @@ export async function POST(request: Request) {
     );
     }
 }
+
+export async function GET() {
+    try {
+        const session = useSession()
+        const user = await session.data?.user
+
+        if(!user) {
+            return NextResponse.json({error: 'Unauthorized'}, {status: 401})
+        } 
+
+        const projects = await prisma.project.findMany({
+            //@ts-ignore
+            where: { userId: user.id }
+        })
+
+        return NextResponse.json(projects, {status: 200})
+    } catch (error) {
+        return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
+    }
+}
